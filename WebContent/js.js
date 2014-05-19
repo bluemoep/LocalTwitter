ae4845f6a7a1762a68f64b8b46e9d3c3dd2feb19
Simon Kurka <dev@simon.kurka.cc>
2014-05-19 19:18:17 +0200

    Zeichenlimit

--- WebContent/js.js
+++ WebContent/js.js
@@ -1,16 +1,23 @@
-google.maps.event.addDomListener(window, 'load', function() {
+$(document).ready(function() {
+	
+	$('#messagebox textarea').on('input propertychange', function() {
+	    $('#lettersleft').html($(this).attr('maxlength') - $(this).val().length);
+	});
+	$('#messagebox textarea').trigger('input');
+	
 	var map = new LTmap();
-	
-	$('#overlay .nojs').hide();
-	$('#overlay .search').show();
-	// TODO: Geolocation
-	// onsuccess: $('#overlay').hide();
-	// onfailure: $('#overlay .search').hide();
-	// onfailure: $('#overlay .notfound').show();
-	
 	map.addMessage({
 		lat : LTmap.lat,
 		lng : LTmap.lng,
 		content : 'Hallo Welt!'
 	});
+	
+	$('#overlay .nojs').hide();
+	$('#overlay .search').show();
+	// TODO: Geolocation
+	// onsuccess: 
+	$('#overlay').hide();
+	// onfailure: $('#overlay .search').hide();
+	// onfailure: $('#overlay .notfound').show();
+	
 });
