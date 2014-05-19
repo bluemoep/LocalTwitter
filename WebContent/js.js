1fe4231a00ff3bcdb67bce9aec5825b154fd888e
Simon Kurka <dev@simon.kurka.cc>
2014-05-19 18:17:00 +0200

    Overlays jQuery eingebunden

--- WebContent/js.js
+++ WebContent/js.js
@@ -1,5 +1,13 @@
 google.maps.event.addDomListener(window, 'load', function() {
 	var map = new LTmap();
+	
+	$('#overlay .nojs').hide();
+	$('#overlay .search').show();
+	// TODO: Geolocation
+	// onsuccess: $('#overlay').hide();
+	// onfailure: $('#overlay .search').hide();
+	// onfailure: $('#overlay .notfound').show();
+	
 	map.addMessage({
 		lat : LTmap.lat,
 		lng : LTmap.lng,
