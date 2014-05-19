668ad31b5ea0b01a49abaa2f5b962e73dcce56b0
Simon Kurka <dev@simon.kurka.cc>
2014-05-19 16:43:26 +0200

    Navigationselemente hinter der MessageBox hervorgeholt

--- WebContent/LTmap.js
+++ WebContent/LTmap.js
@@ -1,38 +1,49 @@
 function LTmap() {
-	
+
 	// Singleton
 	if (typeof LTmap.instance === 'object') {
-        return LTmap.instance;
-    }
+		return LTmap.instance;
+	}
 	LTmap.instance = this;
-	
+
 	// Defaults
 	LTmap.elementId = 'map';
 	LTmap.lat = '53.147086';
 	LTmap.lng = '8.180434';
 	LTmap.zoom = 4;
-	
+
 	// Initialize map with defaults
 	var map = new google.maps.Map(document.getElementById(LTmap.elementId), {
 		zoom : LTmap.zoom,
 		center : new google.maps.LatLng(LTmap.lat, LTmap.lng),
-		mapTypeId : google.maps.MapTypeId.HYBRID
+		mapTypeId : google.maps.MapTypeId.HYBRID,
+		minZoom : 4,
+		maxZoom : 18,
+		zoomControlOptions : {
+			position : google.maps.ControlPosition.RIGHT_TOP,
+		},
+		streetViewControlOptions : {
+			position : google.maps.ControlPosition.RIGHT_TOP,
+		},
+		panControlOptions : {
+			position : google.maps.ControlPosition.RIGHT_TOP,
+		},
 	});
-	
+
 	this.addMessage = function(message) {
 		var marker = new google.maps.Marker({
 			position : new google.maps.LatLng(message.lat, message.lng),
 			map : map,
 			title : 'Zum Öffnen klicken!',
 		});
-		
+
 		marker.message = new google.maps.InfoWindow({
 			content : message.content,
 		});
-		
+
 		google.maps.event.addListener(marker, 'click', function() {
 			marker.message.open(map, marker);
 		});
 	};
-	
+
 }
