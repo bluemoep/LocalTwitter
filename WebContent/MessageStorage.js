1ea68cd9245e3960160e28ad96fdbca0fbc77e73
Simon Kurka <dev@simon.kurka.cc>
2014-05-22 17:33:03 +0200

    MessageStorage untested

---
+++ WebContent/MessageStorage.js
@@ -0,0 +1,79 @@
+/**
+ * @DEPRECATED
+ */
+
+function MessageStorage() {
+	
+	// Singleton
+	if (typeof MessageStorage.instance === 'object') {
+		return MessageStorage.instance;
+	}
+	MessageStorage.instance = this;
+	
+	var latestMessage = localStorage.getItem('messages');
+	
+	var flush = function() {
+		localStorage.setItem('messages', latestMessage);
+	};
+	
+	var flushTimeout = null;
+	var flushTimer = function() {
+		clearTimeout(flushTimeout);
+		flushTimeout = setTimeout(flush, 1000);
+	};
+	
+	// Cleanup
+	setTimeout(function() {
+		var cut = new Date();
+		cut.setDate(cut.getDate()-14);
+		
+		var currentMessage = latestMessage;
+		var lastCurrentMessage = null;
+		while(currentMessage.createdAt > cut) {
+			lastCurrentMessage = currentMessage;
+			currentMessage = currentMessage.next;
+		}
+		lastCurrentMessage.next = null;
+		
+		flushTimer();
+	}, 10000);
+	
+	this.addMessage = function(tweet) {
+		var message = {
+			id : tweet.id,
+			createdAt : new Date(tweet.created_at),
+			readStatus : false,
+			tweet : tweet,
+			next : null
+		};
+		
+		var currentMessage = latestMessage;
+		var lastCurrentMessage = null;
+		while(currentMessage.createdAt > message.createdAt) {
+			lastCurrentMessage = currentMessage;
+			currentMessage = currentMessage.next;
+		}
+		
+		// Check if message exists in localStorage
+		var currentMessage2 = currentMessage;
+		while(currentMessage2.createdAt == message.createdAt) {
+			if(currentMessage2.id == message.id)
+				return;
+			currentMessage2 = currentMessage2.next;
+		}
+		
+		message.next = currentMessage;
+		// Check if this is the latest message, otherwise this is lastCurrentMessage's next message
+		if(lastCurrentMessage == null)
+			latestMessage = message;
+		else
+			lastCurrentMessage.next = message;
+		
+		flushTimer();
+	};
+	
+	this.getLatestMessage = function() {
+		return latestMessage;
+	};
+	
+}
