b5fc2f7e737bde18e4b90520b88baae8dce7b682
Simon Kurka <dev@simon.kurka.cc>
2014-07-08 13:50:59 +0200

    TwitterRequest Klasse angewendet

--- src/edu/oldenburg/it/bluemoep/MessageReceiver.java
+++ src/edu/oldenburg/it/bluemoep/MessageReceiver.java
@@ -1,8 +1,6 @@
 package edu.oldenburg.it.bluemoep;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,17 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import oauth.signpost.OAuthConsumer;
-import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
-import oauth.signpost.exception.OAuthCommunicationException;
-import oauth.signpost.exception.OAuthExpectationFailedException;
-import oauth.signpost.exception.OAuthMessageSignerException;
-
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
 
 
 
@@ -40,37 +29,14 @@ public class MessageReceiver extends HttpServlet {
 		String lng = request.getParameter("lng");
 		System.out.println("Message: " + message);
 		System.out.println("Lat: " + lat);
-		System.out.println("Lng: " + lng);	
-		String encoded = "status=" + encode(message)+"&lat="+encode(lat)+"&long="+encode(lng);
-		System.out.println(encoded);
+		System.out.println("Lng: " + lng);
 
-		String oauth_consumer_key = "XiSYzq3ak6ZVrupuJDk1hh9xF";
-		String oauth_consumer_secret = "EyHAg9kX47YGpRkA0brxRkJEZEs4o6zXVmSvWTnrNBVSkXvpsX";
-		String oauth_token = "2558944136-WbSyJGI7iYDD47J6qtZyiwh3Mv7G7jTevqU4RkJ";
-		String oauth_token_secret = "T5v4Oc3fKoddNUnCnNHkq8EQbJwUO8AvwafRKgceijLRr";
+		TwitterRequest tr = new TwitterRequest("https://api.twitter.com/1.1/statuses/update.json");
+		tr.addParameter("status", message);
+		tr.addParameter("lat", lat);
+		tr.addParameter("long", lng);
 
-		OAuthConsumer oAuthConsumer = new CommonsHttpOAuthConsumer(oauth_consumer_key,oauth_consumer_secret);
-		oAuthConsumer.setTokenWithSecret(oauth_token, oauth_token_secret);
-
-		HttpPost httpPost = new HttpPost("https://api.twitter.com/1.1/statuses/update.json?" + encoded);
-
-		try {
-			oAuthConsumer.sign(httpPost);
-		} catch (OAuthMessageSignerException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (OAuthExpectationFailedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (OAuthCommunicationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-
-		// send the request
-		HttpClient httpClient = new DefaultHttpClient();
-		HttpResponse httpResponse = httpClient.execute(httpPost);
+		HttpResponse httpResponse = tr.doRequest();
 		
 		// getting response Messages
 		int statusCode = httpResponse.getStatusLine().getStatusCode();
@@ -88,31 +54,6 @@ public class MessageReceiver extends HttpServlet {
 
 	}
 
-	//Methode zur encodierung von Strings gem. RFC3986
-	private static String encode(String value){
-		String encoded = null;
-		try {
-			encoded = URLEncoder.encode(value, "UTF-8");
-		} catch (UnsupportedEncodingException ignore) {
-		}
-		StringBuilder buf = new StringBuilder(encoded.length());
-		char focus;
-		for (int i = 0; i < encoded.length(); i++) {
-			focus = encoded.charAt(i);
-			if (focus == '*') {
-				buf.append("%2A");
-			} else if (focus == '+') {
-				buf.append("%20");
-			} else if (focus == '%' && (i + 1) < encoded.length()
-					&& encoded.charAt(i + 1) == '7' && encoded.charAt(i + 2) == 'E') {
-				buf.append('~');
-				i += 2;
-			} else {
-				buf.append(focus);
-			}
-		}
-		return buf.toString();
-	}
 }
 
 
--- src/edu/oldenburg/it/bluemoep/TwitterStreaming.java
+++ src/edu/oldenburg/it/bluemoep/TwitterStreaming.java
@@ -3,19 +3,8 @@ package edu.oldenburg.it.bluemoep;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-
-import oauth.signpost.OAuthConsumer;
-import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
-import oauth.signpost.exception.OAuthCommunicationException;
-import oauth.signpost.exception.OAuthExpectationFailedException;
-import oauth.signpost.exception.OAuthMessageSignerException;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
 
 public class TwitterStreaming {
 
@@ -32,57 +21,27 @@ public class TwitterStreaming {
 			@Override
 			public void run() {
 				while (true) {
-					String encoded = "locations=" + encode("-180,-90,180,90");
-					String oauth_consumer_key = "XiSYzq3ak6ZVrupuJDk1hh9xF";
-					String oauth_consumer_secret = "EyHAg9kX47YGpRkA0brxRkJEZEs4o6zXVmSvWTnrNBVSkXvpsX";
-					String oauth_token = "2558944136-WbSyJGI7iYDD47J6qtZyiwh3Mv7G7jTevqU4RkJ";
-					String oauth_token_secret = "T5v4Oc3fKoddNUnCnNHkq8EQbJwUO8AvwafRKgceijLRr";
+					TwitterRequest tr = new TwitterRequest(
+							"https://stream.twitter.com/1.1/statuses/filter.json");
+					tr.addParameter("locations", "-180,-90,180,90");
+					HttpResponse httpResponse = tr.doRequest();
 
-					OAuthConsumer oAuthConsumer = new CommonsHttpOAuthConsumer(
-							oauth_consumer_key, oauth_consumer_secret);
-					oAuthConsumer.setTokenWithSecret(oauth_token,
-							oauth_token_secret);
-
-					HttpPost httpPost = new HttpPost(
-							"https://stream.twitter.com/1.1/statuses/filter.json?"
-									+ encoded);
-
+					// getting response Messages
+					int statusCode = httpResponse.getStatusLine()
+							.getStatusCode();
+					System.out.println(statusCode + ":"
+							+ httpResponse.getStatusLine().getReasonPhrase());
+					BufferedReader reader;
 					try {
-						oAuthConsumer.sign(httpPost);
-					} catch (OAuthMessageSignerException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (OAuthExpectationFailedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (OAuthCommunicationException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-					try {
-						// send the request
-						HttpClient httpClient = new DefaultHttpClient();
-						HttpResponse httpResponse;
-						httpResponse = httpClient.execute(httpPost);
-
-						// getting response Messages
-						int statusCode = httpResponse.getStatusLine()
-								.getStatusCode();
-						System.out.println(statusCode
-								+ ":"
-								+ httpResponse.getStatusLine()
-										.getReasonPhrase());
-						BufferedReader reader = new BufferedReader(
-								new InputStreamReader(httpResponse.getEntity()
-										.getContent()));
+						reader = new BufferedReader(new InputStreamReader(
+								httpResponse.getEntity().getContent()));
 
 						TweetSource ts = TweetSource.getInstance();
 						String str;
 						while ((str = reader.readLine()) != null)
 							ts.sendMessage(TweetParser.parse(str));
 
-					} catch (IOException e) {
+					} catch (IllegalStateException | IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
@@ -91,33 +50,6 @@ public class TwitterStreaming {
 
 		});
 		runner.start();
-	}
-
-	// Methode zur encodierung von Strings gem. RFC3986
-	private static String encode(String value) {
-		String encoded = null;
-		try {
-			encoded = URLEncoder.encode(value, "UTF-8");
-		} catch (UnsupportedEncodingException ignore) {
-		}
-		StringBuilder buf = new StringBuilder(encoded.length());
-		char focus;
-		for (int i = 0; i < encoded.length(); i++) {
-			focus = encoded.charAt(i);
-			if (focus == '*') {
-				buf.append("%2A");
-			} else if (focus == '+') {
-				buf.append("%20");
-			} else if (focus == '%' && (i + 1) < encoded.length()
-					&& encoded.charAt(i + 1) == '7'
-					&& encoded.charAt(i + 2) == 'E') {
-				buf.append('~');
-				i += 2;
-			} else {
-				buf.append(focus);
-			}
-		}
-		return buf.toString();
 	}
 
 }
